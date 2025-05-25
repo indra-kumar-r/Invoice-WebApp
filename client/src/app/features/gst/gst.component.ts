713ab7d83628f3d73affc55af5fbd187dc442171
INDRA KUMAR R <indra07kumar@gmail.com>
2025-05-25 19:10:49 +0530

    feat: create components

---
+++ client/src/app/features/gst/gst.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gst',
+  imports: [],
+  templateUrl: './gst.component.html',
+  styleUrl: './gst.component.scss'
+})
+export class GstComponent {
+
+}
