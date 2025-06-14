e51ceca41597f2c4887d7520b7c7415e7ed6f94d
INDRA KUMAR R <indra07kumar@gmail.com>
2025-06-14 18:43:46 +0530

    feat: add view component for invoices

---
+++ client/src/app/features/invoice/view-invoice/view-invoice.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-invoice',
+  templateUrl: './view-invoice.component.html',
+  styleUrl: './view-invoice.component.scss',
+})
+export class ViewInvoiceComponent {}
