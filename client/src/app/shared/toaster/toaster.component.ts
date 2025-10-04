9ea11d2577734c167029a4548eefc105ca765620
INDRA KUMAR R <indra07kumar@gmail.com>
2025-10-05 00:26:59 +0530

    refactor: reduce toaster animation duration for faster feedback

--- client/src/app/shared/toaster/toaster.component.ts
+++ client/src/app/shared/toaster/toaster.component.ts
@@ -21,7 +21,7 @@ import { trigger, transition, style, animate } from '@angular/animations';
       ]),
       transition(':leave', [
         animate(
-          '1000ms ease-in',
+          '500ms ease-in',
           style({ opacity: 0, transform: 'translateY(-10px)' })
         ),
       ]),
